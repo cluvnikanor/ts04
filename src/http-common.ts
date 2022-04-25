954f6048d46c27a0e7cc6d7876b9c9385e6b7baf
Scion Landes <55755526+cluvnikanor@users.noreply.github.com>
2022-04-25 23:44:02 +0300

    15

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const productionBaseUrl = "https://heroku-demo0123.herokuapp.com/api";
+const developmentBaseUrl = "http://localhost:8080/api";
+// const productionBaseUrl = "/api";
+const isDevelopment = false;
+// const isDevelopment = true;
+
 export default axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: isDevelopment ? developmentBaseUrl : productionBaseUrl,
     headers: {
         "Content-type": "application/json"
     }
--- src/services/MandalaService.ts
+++ src/services/MandalaService.ts
@@ -1,9 +1,6 @@
 import http from "../http-common";
 import { User } from "../components/Logins/User";
 
-// const create = (data: ITutorialData) => {
-//     return http.post<ITutorialData>("/tutorials", data);
-
 const login = (email: string, password: string) => {
     return http.get<any>(`/login?email=${email}&password=${password}`);
 }
